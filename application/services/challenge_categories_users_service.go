71dc2630927d8470ad6013dc3b914c08b04c0886
whyy <icarovsilva1@gmail.com>
2024-06-27 21:51:35 -0300

    feat: implement insert method in service

--- application/services/challenge_categories_users_service.go
+++ application/services/challenge_categories_users_service.go
@@ -2,11 +2,13 @@ package services
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/IcaroSilvaFK/developer_academy_mvc/application/http/views"
 	"github.com/IcaroSilvaFK/developer_academy_mvc/application/utils"
 	"github.com/IcaroSilvaFK/developer_academy_mvc/infra/models"
 	"github.com/IcaroSilvaFK/developer_academy_mvc/infra/repositories"
+	"gorm.io/gorm"
 )
 
 type ChallengeCategoriesUsersService struct {
@@ -17,4 +19,44 @@ type ChallengeCategoriesUsersServiceInterface interface {
 	Create(ctx context.Context, input views.ChallengeCategoriesUsersInputView, userId string) *utils.RestErr
 	FindByUserId(ctx context.Context, userId string) ([]*models.ChallengeCategoriesUsersModel, *utils.RestErr)
 	Delete(ctx context.Context, relationId string) *utils.RestErr
+}
+
+func NewChallengeCategoriesUsersService(
+	repo repositories.ChallengeCategoriesUsersRepositoryInterface,
+) ChallengeCategoriesUsersServiceInterface {
+
+	return &ChallengeCategoriesUsersService{repo}
+}
+
+// Create implements ChallengeCategoriesUsersServiceInterface.
+func (c *ChallengeCategoriesUsersService) Create(ctx context.Context, input views.ChallengeCategoriesUsersInputView, userId string) *utils.RestErr {
+
+	inp := models.NewChallengeCategoriesUsersModel(input.CategoryId, userId)
+
+	if err := c.repo.Create(ctx, inp); err != nil {
+
+		if err == gorm.ErrDuplicatedKey {
+			errno := utils.NewBadRequestException(fmt.Sprintf("USER WITH ID %s ALREADY HAS THIS CATEGORY %s FAVORITED", userId, inp.CategoryId))
+
+			return errno
+		}
+
+		errno := utils.NewInternalServerError(nil)
+		utils.Error("Error on insert data in database", err)
+
+		return errno
+	}
+
+	return nil
+
+}
+
+// Delete implements ChallengeCategoriesUsersServiceInterface.
+func (c *ChallengeCategoriesUsersService) Delete(ctx context.Context, relationId string) *utils.RestErr {
+	panic("unimplemented")
+}
+
+// FindByUserId implements ChallengeCategoriesUsersServiceInterface.
+func (c *ChallengeCategoriesUsersService) FindByUserId(ctx context.Context, userId string) ([]*models.ChallengeCategoriesUsersModel, *utils.RestErr) {
+	panic("unimplemented")
 }
--- infra/models/challenge_categories_users_model.go
+++ infra/models/challenge_categories_users_model.go
@@ -1,11 +1,26 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"github.com/IcaroSilvaFK/developer_academy_mvc/infra/utils"
+	"gorm.io/gorm"
+)
 
 type ChallengeCategoriesUsersModel struct {
 	ID         string
 	UserId     string `gorm:"index:idx_catmember"`
 	CategoryId string `gorm:"index:idx_catmember"`
+	ChallengesCategoriesModel
 
 	gorm.Model
+}
+
+func NewChallengeCategoriesUsersModel(
+	catId, userId string,
+) *ChallengeCategoriesUsersModel {
+
+	return &ChallengeCategoriesUsersModel{
+		ID:         utils.NewId(),
+		CategoryId: catId,
+		UserId:     userId,
+	}
 }
